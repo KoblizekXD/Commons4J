025a90b2dbe5b2212846889398f63d09ad8f1819
KoblizekXD <janprokupek@centrum.cz>
2023-02-18 18:48:04 +0100

    Main edit

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,10 +1,5 @@
-import com.koblizek.commons4j.time.DateFormatter;
-import com.koblizek.commons4j.time.TimeFormatter;
-
-import static com.koblizek.commons4j.time.DateFormatter.*;
-
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println(TimeFormatter.predefinedAM());
+
     }
 }
