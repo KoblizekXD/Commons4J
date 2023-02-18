c29de71c78e9798771dfc1c9b94531c9a7897377
KoblizekXD <janprokupek@centrum.cz>
2023-02-18 18:38:22 +0100

    Time Formatter

---
+++ src/main/java/com/koblizek/commons4j/time/TimeFormatter.java
@@ -0,0 +1,37 @@
+package com.koblizek.commons4j.time;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TimeFormatter {
+    private final String splitter;
+    private final Format[] formatting;
+
+    public static Format sec(int length) {
+        return new Format(length, 's');
+    }
+    public static Format hour(int length) {
+        return new Format(length, 'H');
+    }
+    public static Format min(int length) {
+        return new Format(length, 'm');
+    }
+
+    public TimeFormatter(String splitter, Format ... formatting) {
+        this.splitter = splitter;
+        this.formatting = formatting;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Format format : formatting) {
+            builder.append(splitter)
+                    .append(format.toString());
+        }
+        return builder.substring(1);
+    }
+    public String toDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern(toString()));
+    }
+}
